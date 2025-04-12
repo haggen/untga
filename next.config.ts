c25e6988896582b304491a67892a57ce40c8627e
Arthur <arthur@corenzan.com>
2025-04-12 11:45:31 -0300

    Improve comments

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,10 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  // React strict mode helps identify potential problems during development.
   reactStrictMode: true,
+
+  // Silence is golden.
   poweredByHeader: false,
 
   async rewrites() {
@@ -10,6 +13,7 @@ const nextConfig: NextConfig = {
       afterFiles: [],
       fallback: [
         {
+          // Avoid displaying the 404 page for API calls.
           source: "/api/:path*",
           destination: "/api/404",
         },
