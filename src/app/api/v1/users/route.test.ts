c18fb9e7bb9e4e28c1cb5033132ee9b06973043e
Arthur <arthur@corenzan.com>
2025-04-13 15:38:01 -0300

    Test endpoint for creating new accounts

---
+++ src/app/api/v1/users/route.test.ts
@@ -0,0 +1,49 @@
+import { db } from "@/lib/db";
+import { NextRequest } from "next/server";
+import { afterAll, expect, test } from "vitest";
+import { POST } from "./route";
+
+afterAll(async () => {
+  await db.session.deleteMany();
+  await db.user.deleteMany();
+});
+
+const url = new URL("http://localhost:3000/api/v1/users");
+const params = Promise.resolve({});
+const headers = {
+  "content-type": "application/json",
+};
+
+test("bad request", async () => {
+  const request = new NextRequest(url, { method: "POST", headers });
+  const response = await POST(request, { params });
+
+  expect(response.status).toBe(400);
+});
+
+test("validation error", async () => {
+  const request = new NextRequest(url, {
+    method: "POST",
+    headers,
+    body: JSON.stringify({ email: "invalid-email", password: "short" }),
+  });
+
+  const response = await POST(request, { params });
+
+  expect(response.status).toBe(422);
+});
+
+test("success", async () => {
+  const request = new NextRequest(url, {
+    method: "POST",
+    headers,
+    body: JSON.stringify({
+      email: "player@example.com",
+      password: "0123456789abcdef",
+    }),
+  });
+
+  const response = await POST(request, { params });
+
+  expect(response.status).toBe(201);
+});
