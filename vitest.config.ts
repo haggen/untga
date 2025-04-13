234aa94a6cd2d8d92b32f0f41887cc605c95d4eb
Arthur <arthur@corenzan.com>
2025-04-13 10:48:32 -0300

    Check types during tests

--- vitest.config.ts
+++ vitest.config.ts
@@ -3,4 +3,10 @@ import { defineConfig } from "vitest/config";
 
 export default defineConfig({
   plugins: [tsconfigPaths()],
+  test: {
+    // Test should fail if there are type issues.
+    typecheck: {
+      enabled: true,
+    },
+  },
 });
