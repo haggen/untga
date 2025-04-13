ba834bf1b760869f78f779936e3fd0cb3534e12c
Arthur <arthur@corenzan.com>
2025-04-13 10:57:07 -0300

    Add initial tests

---
+++ src/lib/api.test.ts
@@ -0,0 +1,39 @@
+import { NextRequest, NextResponse } from "next/server";
+import { expect, test, vi } from "vitest";
+import { Context, withMiddleware } from "./api";
+
+test("", async () => {
+  const url = new URL("http://localhost");
+  const request = new NextRequest(url);
+  const params = {};
+  const extra = { params: Promise.resolve(params) };
+  const response = new NextResponse();
+
+  const a = vi.fn(async (context: Context<{ a: true }>) => {
+    context.state.a = true;
+    return await context.next();
+  });
+
+  const b = vi.fn(async (context: Context<{ b: true }>) => {
+    context.state.b = true;
+    return await context.next();
+  });
+
+  const c = vi.fn(async () => response);
+  const d = vi.fn(async () => response);
+
+  const chain = withMiddleware(a, b, c, d);
+
+  expect(await chain(request, extra)).toBe(response);
+
+  expect(a).toHaveBeenCalledBefore(b);
+  expect(b).toHaveBeenCalledBefore(c);
+  expect(d).not.toHaveBeenCalled();
+
+  expect(a.mock.calls[0][0]).toEqual({
+    request,
+    params,
+    state: expect.any(Object),
+    next: expect.any(Function),
+  });
+});
