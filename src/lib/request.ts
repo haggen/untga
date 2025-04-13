9cdf641cbdebee40aa7400aed8830c44f054e733
Arthur <arthur@corenzan.com>
2025-04-13 10:49:26 -0300

    Safer request body parsing with appropriate error handling

--- src/lib/request.ts
+++ src/lib/request.ts
@@ -1,18 +1,38 @@
 import { BadRequestError } from "@/lib/error";
 
-export function getRemoteAddr(req: Request) {
+export function getRemoteAddr(request: Request) {
   // I'm assuming it's behind a proxy that sets this header and that I can trust it.
-  return req.headers.get("x-forwarded-for")?.split(",")[0].trim() ?? "";
+  return request.headers.get("x-forwarded-for")?.split(",")[0].trim() ?? "";
 }
 
-export function getUserAgent(req: Request) {
-  return req.headers.get("user-agent") ?? "";
+export function getUserAgent(request: Request) {
+  return request.headers.get("user-agent") ?? "";
 }
 
-export async function getPayload<T = unknown>(req: Request): Promise<T> {
-  try {
-    return await req.json();
-  } catch (error) {
-    throw new BadRequestError(undefined, { cause: error });
+export function getBody(request: Request) {
+  const contentType = request.headers.get("content-type");
+
+  if (!contentType) {
+    throw new BadRequestError("Request is missing content type.");
   }
+
+  if (contentType.includes("application/json")) {
+    try {
+      return request.json();
+    } catch (error) {
+      throw new BadRequestError("Could not parse JSON.", { cause: error });
+    }
+  }
+
+  if (contentType.includes("application/x-www-form-urlencoded")) {
+    try {
+      return request.formData();
+    } catch (error) {
+      throw new BadRequestError("Could not parse form data.", { cause: error });
+    }
+  }
+
+  throw new BadRequestError(
+    `Unsupported content type ${contentType}. Change to application/json or application/x-www-form-urlencoded.`
+  );
 }
