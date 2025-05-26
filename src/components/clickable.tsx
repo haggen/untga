857e3e6b37120c517b95baac5abdee023bb86151
Arthur <arthur@corenzan.com>
2025-05-26 14:22:05 -0300

    Rename Command component to Clickable

---
+++ src/components/clickable.tsx
@@ -0,0 +1,34 @@
+"use client";
+
+import Link from "next/link";
+import { HTMLAttributes, ReactNode } from "react";
+import { StatelessAction } from "~/lib/actions";
+
+type Props = Readonly<
+  HTMLAttributes<HTMLElement> & {
+    href?: string;
+    action?: StatelessAction<FormData, void>;
+  }
+>;
+
+/**
+ * Render either an <a> or <button> element based on the provided props.
+ */
+export function Clickable(props: Omit<Props, "href" | "onClick">): ReactNode;
+export function Clickable(props: Omit<Props, "href" | "action">): ReactNode;
+export function Clickable(props: Omit<Props, "action">): ReactNode;
+export function Clickable({ action, href, onClick, ...props }: Props) {
+  if (href) {
+    return <Link href={href} onClick={onClick} {...props} />;
+  }
+
+  if (onClick) {
+    return <button type="button" onClick={onClick} {...props} />;
+  }
+
+  if (action) {
+    return <button type="submit" formAction={action} {...props} />;
+  }
+
+  throw Error("Either 'onClick', 'action' or 'href' prop must be provided.");
+}
