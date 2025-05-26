8e201e62a3c40a37909d7a25724b3e578f8dffeb
Arthur <arthur@corenzan.com>
2025-05-26 14:22:16 -0300

    Refactor Back component

--- src/components/back.tsx
+++ src/components/back.tsx
@@ -2,19 +2,25 @@
 
 import { useRouter } from "next/navigation";
 import { HTMLAttributes, MouseEvent } from "react";
+import { Clickable } from "~/components/clickable";
 import { Slot } from "~/components/slot";
 
 export function Back({
   asChild,
+  children,
   ...props
 }: HTMLAttributes<HTMLElement> & { asChild?: boolean }) {
-  const Component = asChild ? Slot : "a";
+  const Component = asChild ? Slot : Clickable;
   const router = useRouter();
 
-  const onClick = (event: MouseEvent) => {
+  const onClick = (event: MouseEvent<HTMLElement>) => {
     event.preventDefault();
     router.back();
   };
 
-  return <Component onClick={onClick}>{props.children}</Component>;
+  return (
+    <Component onClick={onClick} {...props}>
+      {children}
+    </Component>
+  );
 }
