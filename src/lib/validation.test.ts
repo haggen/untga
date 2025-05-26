4f3be54e8ba968a81c83e53850333dc844801f5f
Arthur <arthur@corenzan.com>
2025-05-26 10:39:22 -0300

    Test parse()

---
+++ src/lib/validation.test.ts
@@ -0,0 +1,32 @@
+import { expect, test } from "vitest";
+import z from "zod";
+import { parse } from "./validation";
+
+test("parse(FormData)", () => {
+  const formData = new FormData();
+  formData.append("value", "24");
+  const data = parse(formData, {
+    value: z.coerce.number().positive(),
+  });
+  expect(data).toEqual({
+    value: 24,
+  });
+});
+
+test("parse(URLSearchParams)", () => {
+  const searchParams = new URLSearchParams();
+  searchParams.append("value", "24");
+  const data = parse(searchParams, {
+    value: z.coerce.number().positive(),
+  });
+  expect(data).toEqual({
+    value: 24,
+  });
+});
+
+test("parse(object)", () => {
+  const data = parse({ value: "24" }, { value: z.coerce.number().positive() });
+  expect(data).toEqual({
+    value: 24,
+  });
+});
