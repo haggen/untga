9fecf18da0e6b3eb8d3b243beb45add2fa08c72f
Arthur <arthur@corenzan.com>
2025-05-26 10:22:48 -0300

    Refactor serialization
    Updated type definitions for serializable types to improve clarity and ensure consistent handling of various data types during serialization

--- src/lib/serializable.ts
+++ src/lib/serializable.ts
@@ -1,32 +1,42 @@
-// type JSON =
-//   | string
-//   | number
-//   | boolean
-//   | null
-//   | undefined
-//   | JSON[]
-//   | { [key: string]: JSON };
+type Scalar = string | number | boolean | null | undefined;
 
 /**
- * The serialized equivalence of a type.
+ * The serializable version of a type.
  * @see https://react.dev/reference/rsc/use-server#serializable-parameters-and-return-values
  */
-export type Serialized<T> = T extends string | number | boolean | null
+export type Serializable<T> = T extends Scalar
   ? T
   : T extends bigint
   ? string
   : T extends { toJSON(): infer U }
   ? U
   : T extends ArrayLike<infer U>
-  ? Serialized<U>[]
+  ? Serializable<U>[]
   : T extends object
-  ? { [K in keyof T]: Serialized<T[K]> }
+  ? { [K in keyof T]: Serializable<T[K]> }
   : never;
 
+function reviver(key: string, value: unknown) {
+  if (value === undefined) {
+    return undefined;
+  }
+  return value;
+}
+
+function replacer(key: string, value: unknown) {
+  if (typeof value === "bigint") {
+    return value.toString();
+  }
+  return value;
+}
+
 /**
- * Produce the serialize equivalence of a value.
+ * Produce the serializable version of a value.
  * @see https://react.dev/reference/rsc/use-server#serializable-parameters-and-return-values
  */
-export function serialize<T>(value: T): Serialized<T> {
-  return JSON.parse(JSON.stringify(value));
+export function serializable<T>(value: T): Serializable<T> {
+  if (value === undefined) {
+    throw new Error("Cannot serialize undefined value");
+  }
+  return JSON.parse(JSON.stringify(value, replacer), reviver);
 }
