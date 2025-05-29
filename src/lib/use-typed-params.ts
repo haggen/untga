483106006fd502d786f0865630de95ffb281bc41
Arthur <arthur@corenzan.com>
2025-05-29 16:01:52 -0300

    Delete dead code

--- src/lib/use-typed-params.ts
+++
@@ -1,7 +0,0 @@
-import { useParams } from "next/navigation";
-import z, { ZodRawShape } from "zod";
-
-export function useTypedParams<T extends ZodRawShape>(shape: T) {
-  const params = useParams();
-  return z.object(shape).parse(params);
-}
