b2bb2377b09203a757615113f5f770c92aaf4e76
Arthur <arthur@corenzan.com>
2025-05-29 16:00:09 -0300

    Support setting headers when throwing an HttpError

--- src/lib/error.ts
+++ src/lib/error.ts
@@ -1,36 +1,42 @@
+type HttpErrorOptions = {
+  headers?: HeadersInit;
+};
+
 export class HttpError extends Error {
   status: number;
+  headers?: HeadersInit;
 
   constructor(
     status = 500,
     message = "Unexpected error.",
-    options?: ErrorOptions
+    options?: ErrorOptions & HttpErrorOptions
   ) {
     super(message, options);
     this.status = status;
+    this.headers = options?.headers;
   }
 }
 
 export class BadRequestError extends HttpError {
-  constructor(message?: string, options?: ErrorOptions) {
+  constructor(message?: string, options?: ErrorOptions & HttpErrorOptions) {
     super(400, message ?? "Request is malformed.", options);
   }
 }
 
 export class ForbiddenError extends HttpError {
-  constructor(message?: string, options?: ErrorOptions) {
+  constructor(message?: string, options?: ErrorOptions & HttpErrorOptions) {
     super(403, message ?? "That is forbidden.", options);
   }
 }
 
 export class UnauthorizedError extends HttpError {
-  constructor(message?: string, options?: ErrorOptions) {
+  constructor(message?: string, options?: ErrorOptions & HttpErrorOptions) {
     super(401, message ?? "You are not authorized to do that.", options);
   }
 }
 
 export class NotFoundError extends HttpError {
-  constructor(message?: string, options?: ErrorOptions) {
+  constructor(message?: string, options?: ErrorOptions & HttpErrorOptions) {
     super(404, message ?? "A required record was not found.", options);
   }
 }
